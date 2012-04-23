2938f0f6d693d5ae230150fcdde959c9edb0150d
Picasso <picasso75@yandex.ru>
2012-04-24 00:23:20 +0400

    add middleware for fixing filter state, add StreetAdmin

--- realestate/estatebase/admin.py
+++ realestate/estatebase/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from models import Region, Locality, Microdistrict, Street
 
+class StreetAdmin(admin.ModelAdmin):
+    list_filter = ('locality',)
+    raw_id_admin = ('locality',)
+    search_fields = ['name',]
+
 admin.site.register(Region)
 admin.site.register(Locality)
 admin.site.register(Microdistrict)
-admin.site.register(Street)
+admin.site.register(Street, StreetAdmin)
--- realestate/settings.py
+++ realestate/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'middleware.FilterPersistMiddleware'
 )
 
 ROOT_URLCONF = 'realestate.urls'
