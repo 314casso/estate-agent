fc254bf67cbdc7564919f8214f96f38cb7c603eb
Picasso <picasso75@yandex.ru>
2012-04-27 01:01:24 +0400

    Base templete with accordion

---
+++ realestate/estatebase/static/js/common_ui.js
@@ -0,0 +1,3 @@
+ $(document).ready(function() {
+   $("#estate_nav").accordion();   
+ });
--- realestate/estatebase/views.py
+++ realestate/estatebase/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 from models import EstateTypeCategory
 
 class EstateTypeView(TemplateView):    
-    template_name = 'base.html'    
+    template_name = 'index.html'    
     def get_context_data(self, **kwargs):
         context = super(EstateTypeView, self).get_context_data(**kwargs)
         estate_categories = EstateTypeCategory.objects.all()        
