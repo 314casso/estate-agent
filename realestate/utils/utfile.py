7e00a01fac214f0da39607a69f8f86787878955f
picasso <picasso75@yandex.ru>
2012-05-30 16:58:13 +0400

    fixture helper

---
+++ realestate/utils/utfile.py
@@ -0,0 +1,26 @@
+'''
+Created on 30.05.2012
+
+@author: picasso
+'''
+
+import settings
+import os
+
+FIXTURE_ROOT = os.path.join(settings.SITE_ROOT, 'estatebase', 'fixtures')
+
+'''
+
+manage.py dumpdata estatebase.estatetypecategory estatebase.estatetype > estatebase/fixtures/temp.json
+
+'''
+
+fixtures = ('temp.json',)
+
+try:
+    for fix in fixtures:
+        fixfile = os.path.join(FIXTURE_ROOT, fix)
+        utf_content = open(fixfile, 'rb').read().decode("unicode_escape").encode("utf8")
+        open(fixfile, 'wb').write(utf_content)
+finally:
+    print 'Done!'
