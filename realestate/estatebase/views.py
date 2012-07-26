b5b410febabab92765a400e0118bda5f67d6dd7d
picasso <picasso75@yandex.ru>
2012-07-26 17:06:36 +0400

    fix

--- realestate/estatebase/views.py
+++ realestate/estatebase/views.py
@@ -205,7 +205,7 @@ class EstateListView(ListView):
     paginate_by = 10
     def get_queryset(self):        
         #q = Estate.objects.all().select_related().prefetch_related('clients__origin','clients__client_type','clients__history','bidgs')
-        q = Estate.objects.all().select_related('region','locality','microdistrict','street').prefetch_related('bidgs').all()
+        q = Estate.objects.all().select_related('region','locality','microdistrict','street','estate_type','history','estate_status').prefetch_related('bidgs').all()
         return q
     def get_context_data(self, **kwargs):
         context = super(EstateListView, self).get_context_data(**kwargs)        
