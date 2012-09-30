efc07f34793e1fe944d8a2bab859ad3abb0d17a5
Picasso <picasso75@yandex.ru>
2012-09-30 23:09:56 +0400

    test

--- realestate/settings.py
+++ realestate/settings.py
@@ -9,7 +9,7 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-from local_settings import * #@UnusedWildImport
+#from local_settings import * #@UnusedWildImport
 
 LOGIN_REQUIRED_URLS = (
     r'/estatebase/(.*)$',    
