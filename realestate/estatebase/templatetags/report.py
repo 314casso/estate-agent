b850a8d6deeb13a8a8addd89e677a3801c098787
Picasso <picasso75@yandex.ru>
2012-10-10 00:28:22 +0400

    fix

--- realestate/estatebase/templatetags/report.py
+++ realestate/estatebase/templatetags/report.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import template
 from django.contrib.humanize.templatetags.humanize import intcomma
+from estatebase.wrapper import get_wrapper
+from collections import OrderedDict
 
 register = template.Library()
 
@@ -27,3 +29,24 @@ def communication(estate):
     if estate.driveway:
         comms.append('%s: %s%s' %  (u'подъезд', estate.driveway.name.lower(), distance_helper(estate.driveway_distance)))    
     return {'comms': comms}
+
+@register.inclusion_tag('inclusion/comma_from_details.html')
+def wrapper_fieldset_comma(bidg, fieldset_name):
+    return wrapper_fieldset(bidg, fieldset_name)
+
+@register.inclusion_tag('inclusion/tr_from_details.html')
+def wrapper_fieldset_tr(bidg, fieldset_name):
+    return wrapper_fieldset(bidg, fieldset_name)
+
+def wrapper_fieldset(bidg, fieldset_name):
+    details = OrderedDict()
+    wrapper = get_wrapper(bidg)
+    field_list = getattr(wrapper, fieldset_name)()    
+    for field in field_list:
+        bidg_field = bidg._meta.get_field(field)
+        value = getattr(bidg,field)
+        if bidg_field.get_internal_type() == 'BooleanField' and value:
+            value = u'Есть'
+        if value:
+            details[wrapper.get_label(field) or bidg_field.verbose_name] = value            
+    return {'details': details}    
--- realestate/estatebase/wrapper.py
+++ realestate/estatebase/wrapper.py
@@ -11,21 +11,25 @@ class FieldWrapper():
         self.name = name
         self.label = label    
 
-class BaseWrapper(object):
+class BaseWrapper(object):    
     field_set = None    
     def __init__(self):
         for f in self.get_exclude_set():
             try:             
                 self.field_set.remove(f)
             except:
-                pass              
+                pass
     def field_list(self):                
+        return self.field_set
+    def exterior_list(self):
         return self.field_set
     def interior_list(self):
         return self.interior_set
     def get_exclude_set(self):
         return []
-               
+    def get_label(self, field):
+        return getattr(self, field, None)
+                   
 class BidgWrapper(BaseWrapper):    
     field_set = ['estate_type', 'room_number', 'year_built', 'floor', 'floor_count', 'elevator', 'wall_construcion', 'exterior_finish', 'window_type', 'roof', 'heating', 'ceiling_height', 'room_count', 'total_area', 'used_area', 'wall_finish', 'flooring', 'ceiling', 'interior', 'basic']    
     interior_set = ['wall_finish', 'flooring', 'ceiling', 'interior']
@@ -48,7 +52,7 @@ class HomeWrapper(BidgWrapper):
     pass
 
 class SteadWrapper(BaseWrapper):
-    field_set = ['id', 'estate', 'total_area', 'face_area', 'shape', 'land_type', 'purpose']
+    field_set = ['estate_type', 'total_area', 'face_area', 'shape', 'land_type', 'purpose']
     #    land_type = u'Земля ТЕСТ'
     def get_exclude_set(self):
         return []
@@ -74,4 +78,4 @@ def get_wrapper(obj):
     if type(obj).__name__ == 'Bidg':
         return WRAPPERS[obj.estate_type.template][0]
     elif type(obj).__name__ == 'Stead':        
-        return WRAPPERS[obj.estate.estate_type.template][1]
+        return WRAPPERS[obj.estate_type.template][1]
