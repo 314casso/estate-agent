d6e409d969266b2557bd314f6f42d52737798188
picasso <picasso75@yandex.ru>
2012-12-12 16:59:22 +0400

    migration

--- realestate/maxim_base/models.py
+++ realestate/maxim_base/models.py
@@ -106,10 +106,11 @@ class RealEstate(models.Model):
 #        if self.creator_id:
 #            return UserUser.objects.get(pk=self.creator_id).user_id
 #
-#    @property
-#    def i_source_id(self):
-#        if self.source_id:
-#            return SourceOrigin.objects.get(pk=self.source_id or 14).origin_id
+    @property
+    def i_source_id(self):
+        origin_mapper = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 10, 8: 14, 9: 7, 10: 8, 11: 9, 12: 11, 13: 13, 14: 12}
+        if self.source_id in origin_mapper:
+            return origin_mapper[self.source_id]        
 #    
 #    @property
 #    def i_last_editor_id(self):
