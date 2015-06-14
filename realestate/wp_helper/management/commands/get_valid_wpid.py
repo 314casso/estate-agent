fd16278cfbf60528b3174988f58cc7822b0ec6ad
picasso <picasso75@yandex.ru>
2015-06-14 20:17:23 +0300

    fix

--- realestate/wp_helper/management/commands/get_valid_wpid.py
+++ realestate/wp_helper/management/commands/get_valid_wpid.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from estatebase.models import Estate, EstateStatus
+from estatebase.models import Estate
 from django.utils import translation
-from wp_helper.models import EstateWordpressMeta
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         translation.activate('ru')   
         self.get_valid()      
                 
-    def get_valid(self):        
-        estates = Estate.objects.filter(validity__exact=Estate.VALID)       
-        for estate in estates:                        
-            try:
-                wp_meta = estate.wp_meta
-#                 print "pk: %s post_id: %s" % (estate.id, wp_meta.post_id)
-            except EstateWordpressMeta.DoesNotExist:
-                print "pk: %s post_id: %s" % (estate.id, None)
-                
-             
-        print len(estates)
-    
-    
+    def get_valid(self):
+        f = open('/tmp/post_ids.txt','w')
+        post_ids = set(Estate.objects.filter(validity__exact=Estate.VALID).values_list('wp_meta__post_id', flat=True))
+        for post_id in post_ids:
+            f.write('%s\n' % post_id)
+        f.close()
