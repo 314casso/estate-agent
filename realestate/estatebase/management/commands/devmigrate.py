9b360a2e0fed6328206c9866726755d375ff784b
picasso <picasso75@yandex.ru>
2015-07-19 12:06:42 +0300

    fix

--- realestate/estatebase/management/commands/devmigrate.py
+++ realestate/estatebase/management/commands/devmigrate.py
@@ -7,7 +7,8 @@ class Command(BaseCommand):
         self.do_job()
         
     def do_job(self):        
+        EntranceEstate.objects.all().delete()
         q = Estate.objects.filter(entrances__isnull=True)
         q = q.exclude(beside__isnull=True)
         for e in q:            
-            EntranceEstate.objects.create(estate=e,type=1,basic=True,beside=e.beside)
+            EntranceEstate.objects.create(estate=e,type=1,basic=True,beside=e.beside,distance=e.beside_distance)
