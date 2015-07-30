da864e8c0320c91bfb316ce597d706fb6bd9db3e
picasso <picasso75@yandex.ru>
2015-07-30 23:12:21 +0300

    fix

--- realestate/exportdata/custom_makers/restate.py
+++ realestate/exportdata/custom_makers/restate.py
@@ -9,9 +9,9 @@ class Restate(YandexPlusXML):
         MIN_PRICE_LIMIT = 100000  
         f = {
              'validity':Estate.VALID,
-             'history__modificated__gte':self.get_delta(),             
+#              'history__modificated__gte':self.get_delta(),             
              'agency_price__gte': MIN_PRICE_LIMIT,  
-             'street__isnull': False, 
+#              'street__isnull': False, 
              'estate_params__exact': EstateParam.PAYEXPORT,          
              }
         q = Estate.objects.all()
