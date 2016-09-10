c8bc193c9036034c211806e6cdec97403f2dda78
picasso <picasso75@yandex.ru>
2016-09-10 12:05:04 +0300

    migration fix

--- realestate/estatebase/migrations/0001_initial.py
+++ realestate/estatebase/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0009_auto_20160907_2122'),
+        ('auth', '__latest__'),
         ('devrep', '__first__'),
     ]
 
