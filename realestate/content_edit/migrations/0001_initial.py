268c1436ca56568f32afd83fc9df37eec4f55e68
picasso <picasso75@yandex.ru>
2016-10-01 19:52:05 +0300

    fix

--- realestate/content_edit/migrations/0001_initial.py
+++ realestate/content_edit/migrations/0001_initial.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.contrib.sites.managers
-import ckeditor.fields
 import django.db.models.manager
 
 
@@ -19,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('name', models.CharField(max_length=255, unique=True)),
-                ('content', ckeditor.fields.RichTextField(blank=True)),
+                ('content', models.TextField(blank=True)),
                 ('site', models.ForeignKey(to='sites.Site')),
             ],
             managers=[
