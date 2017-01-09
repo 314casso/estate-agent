477d197029a40474e18b9586cbe54ed476bc341e
picasso <picasso75@yandex.ru>
2017-01-09 23:41:35 +0300

    command

--- realestate/estatebase/management/commands/devmigrate.py
+++ realestate/estatebase/management/commands/devmigrate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from estatebase.models import Estate
+from estatebase.models import Estate, Microdistrict
 
 class Command(BaseCommand):    
     def handle(self, *args, **options):
@@ -8,10 +8,16 @@ class Command(BaseCommand):
         
     def do_job(self):        
 #         estate_number=u'0',
-        q = Estate.objects.filter(estate_number=u'0', address_state__isnull=True)
+        q = Estate.objects.filter(microdistrict__name__startswith=u'неправ')
         print len(q)
-        for item in q:            
+        for item in q:
+            try:
+                m = Microdistrict.objects.get(name=u'жилой район', locality=item.locality)
+#                 print m 
+            except:
+                print  item.locality
+#             print item         
 #             item.street = None 
-            item.estate_number = None
-            item.address_state = Estate.NO_NUMBER
-            item.save()
+#             item.estate_number = None
+#             item.address_state = Estate.NO_NUMBER
+#             item.save()
