9e80ee29ef28af01d10caeaee40d9d223c67c3a0
picasso <picasso75@yandex.ru>
2017-03-20 00:39:35 +0300

    fix

--- realestate/wp_helper/admin.py
+++ realestate/wp_helper/admin.py
@@ -53,7 +53,7 @@ class TaxonomyAdminForm(forms.ModelForm):
         widgets = {            
             'localities': AutoCompleteSelectMultipleWidget(lookup_class=LocalityLookup),
         }
-        fields = ['localities', 'wp_meta_locality', 'regions', 'wp_id']
+        fields = ['name', 'localities', 'wp_meta_locality', 'regions', 'wp_id']
 
 class MetaAdminForm(forms.ModelForm):    
     class Meta(object):        
