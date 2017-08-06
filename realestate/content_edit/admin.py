f40ad1cc9b7fcb3cfdffe85db589b5f79fd375ad
picasso <picasso@picasso-kubuntu>
2017-08-06 23:09:17 +0300

    fix

--- realestate/content_edit/admin.py
+++ realestate/content_edit/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from content_edit.models import CmsContent
 
 class CmsContentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'site',)
+    list_display = ('name', 'site', 'content')
     list_filter = ('site',)
     search_fields = ('name','content')
     
