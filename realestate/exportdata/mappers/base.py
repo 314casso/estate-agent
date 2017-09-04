88ed852096fbd12e8fab6c07ad1a17ef668a179a
picasso <picasso@picasso-kubuntu>
2017-09-04 23:19:05 +0300

    fix

--- realestate/exportdata/mappers/base.py
+++ realestate/exportdata/mappers/base.py
@@ -244,7 +244,8 @@ class BaseMapper(object):
             return
         region = u'Краснодарского края' if estate.locality.locality_type_id == Locality.CITY else estate.locality.region.regular_name_gent
         location = u'%s %s' % (estate.locality.name_loct, region)
-        result = u'Продается %s в %s' % (estate.estate_type.name.lower(), location)             
+        estate_type_name =  u'%s' % estate.estate_type.name
+        result = u'Продается %s в %s' % (estate_type_name.lower(), location)             
         return result
     
     @property        
