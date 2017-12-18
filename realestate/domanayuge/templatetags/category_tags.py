7bdca1aee0451e52acb8dc56eb1d0e3dac6f8409
picasso <picasso@picasso-kubuntu>
2017-12-18 23:15:10 +0300

    fix

--- realestate/domanayuge/templatetags/category_tags.py
+++ realestate/domanayuge/templatetags/category_tags.py
@@ -55,6 +55,9 @@ def site_filtered(q, site):
         q = q.filter(tags__contains=geo_tags)        
     else:
         ex_tags = get_all_geo_tags()
+        import logging
+        logger = logging.getLogger('estate')
+        logger.error(u"ex_tags:%s" % ",".join(ex_tags))
         if ex_tags:            
             q = q.exclude(tags__overlap=ex_tags)
     return q
