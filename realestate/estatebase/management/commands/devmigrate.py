d9280b782136fd342332c468f3b5a5f568772c39
picasso <picasso75@yandex.ru>
2019-07-20 19:00:17 +0300

    fix

--- realestate/estatebase/management/commands/devmigrate.py
+++ realestate/estatebase/management/commands/devmigrate.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from estatebase.models import Estate, Microdistrict
+from estatebase.models import Estate, Microdistrict, Contact
 from django.db.models.signals import post_save
+import unicodecsv
+import os
+from settings import MEDIA_ROOT
+
 
 class Command(BaseCommand):    
     def handle(self, *args, **options):
-        self.actualize()
+        self.correct_lots_export()
             
     def actualize(self):
         q = Estate.objects.all()        
         for estate in q:            
             Estate.objects.filter(pk=estate.pk).update(actualized=estate.get_actual_date())
             
-    def do_job(self):        
-#         estate_number=u'0',
+    def do_job(self):      
         q = Estate.objects.filter(microdistrict__name__startswith=u'неправиль')
         print len(q)
         for item in q:
@@ -24,13 +27,51 @@ class Command(BaseCommand):
             try:
                 m = Microdistrict.objects.get(name=u'жилой район', locality=item.locality)
                 item.microdistrict = m
-                item.save()
-#                 print m 
+                item.save() 
             except:
                 print  item.locality
-#             print item         
-#             item.street = None 
-#             item.estate_number = None
-#             item.address_state = Estate.NO_NUMBER
-#             item.save()
-    
+
+
+    def correct_lots_export(self):     
+        estates = Estate.objects.filter(validity_id=Estate.VALID)
+        file_name = os.path.join(MEDIA_ROOT, 'feed' ,'%s.csv' % 'correct_lots_contacts')
+        with open(file_name, "w") as csvfile:
+            writer = unicodecsv.writer(csvfile)
+            writer.writerow(['estate_id',
+            'name1','phone11','phone12','phone13','phone14','phone15','email11','email12','email13','email14','email15',
+            'name2','phone21','phone22','phone23','phone24','phone25','email21','email22','email23','email24','email25',
+            'name3','phone31','phone32','phone33','phone34','phone35','email31','email32','email33','email34','email35',
+            'name4','phone41','phone42','phone43','phone44','phone45','email41','email42','email43','email44','email45',
+            'name5','phone51','phone52','phone53','phone54','phone55','email51','email52','email53','email54','email55',
+            ]
+            )
+            for estate in estates:
+                row = []
+                row.append(estate.pk)        
+                for client in estate.clients.all()[:5]:
+                    row.append(u'%s' % client.name)
+                    phones = set()
+                                            
+                    for contact in client.contacts.filter(contact_type=Contact.PHONE, contact_state__in=[Contact.AVAILABLE, Contact.NOTCHECKED, Contact.NOTRESPONDED])[:5]:                  
+                        phones.add(u'%s' % (contact.contact))                
+                                        
+                    phones = list(phones)
+                    add_empty_cols(phones)                    
+                    row.extend(phones)
+                    
+                    emails = set()
+                    for contact in client.contacts.filter(contact_type=Contact.EMAIL, contact_state__in=[Contact.AVAILABLE, Contact.NOTCHECKED, Contact.NOTRESPONDED])[:5]:                  
+                        emails.add(u'%s' % (contact.contact))                
+                        
+                    emails = list(emails)            
+                    add_empty_cols(emails)
+                    row.extend(emails)    
+                          
+                writer.writerow(row)
+            
+
+def add_empty_cols(lst):
+    cols = 5 - len(lst)
+    if cols > 0:
+        for x in range(cols):
+            lst.append('')  
--- realestate/estatebase/urls.py
+++ realestate/estatebase/urls.py
@@ -24,8 +24,8 @@ from estatebase.views import ClientListView, \
     FileDeleteView, FileUpdateView, GenericFilesView, ManageEstateM2MLinks,\
     global_search, BidReportView, BidFreeListView, bid_calendar_events,\
     events_calendar, ManageM2MEvents, lot_events, create_generic_event,\
-    estate_calendar_events, client_events, client_calendar_events, trigger_event,\
-    correct_lots_export
+    estate_calendar_events, client_events, client_calendar_events, trigger_event
+    
     
 
 urlpatterns = patterns('',   
@@ -164,8 +164,7 @@ urlpatterns += patterns('',
     url (r'^estatelistcontacts/(?P<contact_type_pk>\d+)$', view=estate_list_contacts, name='estate_list_contacts'),
     url (r'^bidlistcontacts/(?P<contact_type_pk>\d+)/(?P<view_pk>\w+)$', view=bid_list_contacts, name='bid_list_contacts'),
     url (r'^clientlistcontacts/(?P<contact_type_pk>\d+)$', view=client_list_contacts, name='client_list_contacts'),
-    url (r'^incorrectcontacts/$', view=incorrect_contacts, name='incorrect_contacts'),
-    url (r'^correctlotsexport/$', view=correct_lots_export, name='correctlotsexport'),
+    url (r'^incorrectcontacts/$', view=incorrect_contacts, name='incorrect_contacts'),    
 )
 
 urlpatterns += patterns('',    
