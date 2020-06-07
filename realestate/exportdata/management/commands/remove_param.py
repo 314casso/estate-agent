df370439ab79d63405cd122fe72a036a7b5f0791
picasso <picasso75@yandex.ru>
2020-06-08 00:01:36 +0300

    fix

--- realestate/exportdata/management/commands/remove_param.py
+++ realestate/exportdata/management/commands/remove_param.py
@@ -4,7 +4,8 @@ from estatebase.models import Estate
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        PARAM_ID = 16
-        q = Estate.objects.filter(estate_params__exact=PARAM_ID)
-        for estate in q:
-            estate.estate_params.remove(PARAM_ID)       
+        PARAM_IDs = [15,4,19,13,8,9,11,12,20]
+        for PARAM_ID in PARAM_IDs: 
+            q = Estate.objects.filter(estate_params__exact=PARAM_ID)
+            for estate in q:
+                estate.estate_params.remove(PARAM_ID)       
