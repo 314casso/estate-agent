872492bbc12e1b98279ae232fa14da2b00f7e7c7
picasso <picasso75@yandex.ru>
2020-08-18 16:15:24 +0300

    fix

--- realestate/wp_helper/management/commands/clear_errorlog.py
+++ realestate/wp_helper/management/commands/clear_errorlog.py
@@ -5,10 +5,18 @@ from estatebase.models import Estate
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.clear_log()
+        self.set_log()
                              
     def clear_log(self):
         error_qs = Estate.objects.filter(wp_meta__status=EstateWordpressMeta.ERROR)
         for estate in error_qs:
             estate.wp_meta.status = EstateWordpressMeta.OUT
             estate.wp_meta.save()
+            
+    
+    def set_log(self):
+        qs = Estate.objects.filter(validity__exact=Estate.VALID, wp_meta__status=EstateWordpressMeta.UPTODATE).count()
+        print qs        
+#         for estate in qs:
+#             estate.wp_meta.status = EstateWordpressMeta.XMLRPC
+#             estate.wp_meta.save()
