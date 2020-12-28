0f9281fffb71fdc5186cddd3dce4d2e878d28d75
picasso <picasso75@yandex.ru>
2020-12-28 22:58:24 +0300

    fix

--- realestate/wp_helper/management/commands/wp_tests.py
+++ realestate/wp_helper/management/commands/wp_tests.py
@@ -21,25 +21,41 @@ class Command(BaseCommand):
         self.sync_terms(wp_service)      
                 
     def sync_terms(self, wp_service):
+        
+        estates = Estate.objects.filter(wp_meta__status=EstateWordpressMeta.ERROR)
+        for estate in estates:        
+            locality_id = estate.locality_id
+            estate_type_name = estate.basic_estate_type.name 
+            wp_cat = WordpressTaxonomyTree.objects.filter(parent__localities__id=locality_id, name__iexact=estate_type_name)[:1]    # @UndefinedVariable
+            if wp_cat:
+                tax = wp_cat.get()
+                print tax.name
+                tax.delete()           
+                
 #         estate = Estate.objects.get(pk='107602')           
 #         print(estate.pk)                     
 #         wp_service.sync_post(estate)
 
-        terms = WordpressTaxonomyTree.objects.filter(localities__isnull=False)
+#         terms = WordpressTaxonomyTree.objects.filter(localities__isnull=True, wp_id__isnull=False)
+#         for term in terms:
+#             try:
+#                 l = wp_service.client.call(taxonomies.GetTerm('category', term.wp_id))
+#             except:
+#                print term.name            
         
-        for term in terms:
-            l = wp_service.client.call(taxonomies.GetTerms('category', {'search': term.name }))
-            if len(l) == 1:
-                if l[0].id != term.wp_id:
-                    if l[0].parent == term.parent.wp_id and term.name == l[0].name:
-                        term.wp_id = l[0].id
-                        term.save()                   
-#                         print u"%s %s %s" % (term.name, l[0].id, term.wp_id)
-                    else:
-                        print u'Error %s %s %s' % (term.name, l[0].parent, l[0].name)
-                else:
-                    print u'Success %s %s %s' % (term.name, l[0].parent, l[0].name)
-            elif len(l) == 0: 
-                wp_service.create_taxonomy(term.parent.wp_id, term.name)       
-            else:
-                print u"%s %s!!!" % (term.name, len(l))
+#         for term in terms:
+#             l = wp_service.client.call(taxonomies.GetTerms('category', {'search': term.name }))
+#             if len(l) == 1:
+#                 if l[0].id != term.wp_id:
+#                     if l[0].parent == term.parent.wp_id and term.name == l[0].name:
+#                         term.wp_id = l[0].id
+#                         term.save()                   
+# #                         print u"%s %s %s" % (term.name, l[0].id, term.wp_id)
+#                     else:
+#                         print u'Error %s %s %s' % (term.name, l[0].parent, l[0].name)
+#                 else:
+#                     print u'Success %s %s %s' % (term.name, l[0].parent, l[0].name)
+#             elif len(l) == 0: 
+#                 wp_service.create_taxonomy(term.parent.wp_id, term.name)       
+#             else:
+#                 print u"%s %s!!!" % (term.name, len(l))
