baa1b47ae02ea13e973fb05095994ab531408b80
picasso <picasso75@yandex.ru>
2021-05-27 00:34:00 +0300

    fix

--- realestate/exportdata/engines/cian.py
+++ realestate/exportdata/engines/cian.py
@@ -35,7 +35,7 @@ class CianEngine(BaseEngine):
         
         address = mapper.address 
         address_parts = []
-        for field in ['region', 'locality', 'district', 'sub_locality', 'street']:
+        for field in ['region', 'locality', 'district', 'street']:
             value = getattr(address, field)
             if value and not value in address_parts:
                 address_parts.append(value)                                         
